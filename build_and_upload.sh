638aeb0e4cd5e2576e98610692b9abc9c07644ed
Matthias Mittner <matthias.mittner@uit.no>
2015-09-25 11:25:37 +0200

    travis 3

---
+++ build_and_upload.sh
@@ -0,0 +1,16 @@
+# get conda info about root_prefix and platform
+function conda_info {
+    conda info --json | python -c "import json, sys; print(json.load(sys.stdin)['$1'])"
+}
+
+CONDA_ENV=$(conda_info root_prefix)
+PLATFORM=$(conda_info platform)
+BUILD_PATH=$CONDA_ENV/conda-bld/$PLATFORM
+
+for f in `ls | grep r-`;
+do 
+
+conda build $f; 
+conda install $f --use-local; 
+anaconda -t ${ANACONDA_TOKEN} upload $BUILD_PATH/$f*.tar.bz2
+done
